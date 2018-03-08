78b85245b6ba766eda01a217279487af96af069f
mcw <18707746@qq.com>
2018-03-08 15:37:29 +0800

    # -*- coding: utf-8 -*-：

--- chapter 2/Insertion sort.py
+++ chapter 2/Insertion sort.py
@@ -1,15 +1,21 @@
+# -*- coding: utf-8 -*-：
+
+'''increasing order'''
 def insertion_sort(x):
-    x = list(str(x))
+    if type(x) == int:
+        x = list(str(x))
 
     for j in range(1,len(x)):
         key = x[j]
         i = j - 1
         temp = ['']
-        while i >= 0 and x[i] > key:
+        while i >= 0 and x[i] > key: #如果降序，则将大于改为小于
             temp[0] = x[i+1]
             x[i+1] = x[i]
             x[i] = temp[0]
             i -= 1
     return x
 
-print(insertion_sort(987654321))
+
+print(insertion_sort([31,41,59,26,41,58]))
+print(insertion_sort(123654))
